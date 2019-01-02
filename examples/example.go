d35fb960768fc736c6cad5bbc9684ccf871f5929
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2019-01-02 17:18:28 +0530

    Format example

--- examples/example.go
+++ examples/example.go
@@ -5,12 +5,11 @@ import (
 	"github.com/clita/diff"
 )
 
-
 func main() {
 	a := "Hello Vaibhav\n\n\n how are you doing tnight, you must be doing good"
 	b := "Hello Vaibhav how are you doing night, you must be doing  good"
-	
+
 	// string1, string2 := diff.FindColouredChanges(a, b, "lines")
 	string1, string2 := diff.FindColouredChanges(a, b, "words")
-	fmt.Println("String1: \n" + string1 + "\n\n" + "String2: \n" +  string2)
+	fmt.Println("String1: \n" + string1 + "\n\n" + "String2: \n" + string2)
 }
